966d599ef18bb072b4c7521864350d214a2169de
Md Muhtasim Hossain <111693808+muhtasiim@users.noreply.github.com>
2023-10-13 03:23:52 +0600

    Update Simple Calculator.py

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -19,8 +19,8 @@ print("4. Divide")
 
 choice = input("Enter choice (1, 2, 3, 4): ")
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
 if choice == '1':
     result = add(num1, num2)
